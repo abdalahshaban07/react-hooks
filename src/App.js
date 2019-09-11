803cb10972cdd5c60bff8fdc5bbffc2f1042bda8
abdalah <abdalahshaban@yahoo.com>
2019-09-11 03:52:55 +0200

    use state hooks with array

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import HookCounterFour from './components/HookCounterFour';
 
 function App() {
-	return <div className="App" />;
+	return (
+		<div className="App">
+			<HookCounterFour />
+		</div>
+	);
 }
 
 export default App;
