08678cd1674b66dd23e4134c4b7d3cddba9736f6
abdalah <abdalahshaban@yahoo.com>
2019-09-11 03:25:28 +0200

    use prev state using useState Hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import './App.css';
+import HookCounterTwo from './components/HookCounterTwo';
 
 function App() {
-	return <div className="App" />;
+	return (
+		<div className="App">
+			<HookCounterTwo />
+		</div>
+	);
 }
 
 export default App;
