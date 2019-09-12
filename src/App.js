53c1b50a931d558523a8b43a6eab0712ed90d5f0
abdalah <abdalahshaban@yahoo.com>
2019-09-12 23:33:14 +0200

    use reducer with simple and complex state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import './App.css';
+import CounterOne from './components/CounterOne';
+import CounterTwo from './components/CounterTwo';
+import CounterThree from './components/CounterThree';
 
 function App() {
-	return <div className="App" />;
+	return (
+		<div className="App">
+			{/*<CounterOne />*/}
+			{/*<CounterTwo />*/}
+			<CounterThree />
+		</div>
+	);
 }
 
 export default App;
